99d72a3566a6db96089543f0983eaf8e277ebc7d
markiniv <33001862+markiniv@users.noreply.github.com>
2018-10-03 01:51:44 +0300

    Add files via upload

--- task_2.sh
+++ task_2.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 if [ "$2" == "domain" ]; then
-	echo $3
 	if [ ! -d "$1" ]; then
     	mkdir $1
 	fi
-	`whois $3 >> $1/github_com.html`
+	part_0=${3#h*//}		
+	part_1=${part_0#w*.}
+	part_2=${part_1%.*}
+	site='https://www.whois.com/whois/'${part_1}
+	final=${part_2#w*.}'_'${3##*.}
+	echo $site
+	curl -o $1/$final.html $site
 elif [ "$2" == "file" ]; then
 	if [ ! -d "$1" ]; then
 		mkdir $1
 	fi
 	for domain in `cat $3`; do
-	   echo $domain
-	   `whois $domain >> $1/github_com.html`
+		part_0=${domain#h*//}
+		part_1=${part_0#w*.}
+		part_2=${part_1%.*}
+		site='https://www.whois.com/whois/'${part_1}
+		final=${part_2#w*.}'_'${domain##*.}'.html'
+		echo $site
+		curl -o $1/$final $site
 	done
 fi
